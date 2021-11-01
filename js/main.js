6acc129352c0c3e01c3e08f91f646d91fe584570
AxelDuval <axel.duval@gmail.com>
2021-11-01 21:23:38 +0100

    Rearrange interface for tests

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const cards = document.querySelectorAll('.card');
+let cardActive = false;
+let firstCard;
+let secondCard;
+let lockCards = false;
+
+
+for (card of cards){
+    this.addEventListener('click', test);
+}
+
+function test(){
+
+}
