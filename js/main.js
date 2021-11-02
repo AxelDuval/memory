83bfc24e19474441e345f24543bc8c095b1adb3d
unknown <axel.duval@gmail.com>
2021-11-02 16:50:53 +0100

    Create flip on card and store the card value

--- js/main.js
+++ js/main.js
@@ -3,12 +3,26 @@ let cardActive = false;
 let firstCard;
 let secondCard;
 let lockCards = false;
+let array = [];
 
 
-for (card of cards){
-    this.addEventListener('click', test);
+
+for (let card of cards){
+    card.addEventListener('click', revealCard);   
 }
 
-function test(){
 
-}
+function revealCard(){
+    this.childNodes[1].classList.add('active');
+      let attribute = this.getAttribute('data-attribute');
+      array.push(attribute);
+      firstCard = array[0];
+      secondCard = array[1];
+      checkCards(firstCard, secondCard);
+}      
+
+function checkCards(firstCard, secondCard){
+    console.log(firstCard);
+    console.log(secondCard);
+}; 
+
